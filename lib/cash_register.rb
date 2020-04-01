c139d191d9ab4918367555c614c82d61630ae0e3
Heeraj Ramsanai <ramsanai_heeraj@live.com>
2020-04-01 00:15:40 +0000

    Automatically backed up by Learn

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -1,13 +1,14 @@
 
 class CashRegister
-  attr_accessor :total, :discount
+  attr_accessor :total, :discount :item :transaction
 
   def initialize(discount = 0)
     @total = 0
     @discount = discount
+    @item = []
   end
 
-  def add_item(title,price,total)
+  def add_item(title,price,total= 1)
 
   end
 
