f70936f863da4aed01ff3e3027d679844a347ff9
Heeraj Ramsanai <ramsanai_heeraj@live.com>
2020-04-01 01:53:13 +0000

    Automatically backed up by Learn

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -17,9 +17,7 @@ class CashRegister
 
   def add_item_2(title, price, quantity = 1)
    self.total = price * quantity
- end
  
-
   def apply_discount
     if discount != 0
     self.total = (total * ((100.0 - discount.to_f)/100)).to_i
