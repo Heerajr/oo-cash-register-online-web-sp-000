30f87abc7244de9026179ac8e99fca5e1a460fa9
Heeraj Ramsanai <ramsanai_heeraj@live.com>
2020-04-01 00:18:39 +0000

    Automatically backed up by Learn

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -1,6 +1,6 @@
 
 class CashRegister
-  attr_accessor :total, :discount :items :transaction
+  attr_accessor :total, :discount, :items, :transaction
 
   def initialize(discount = 0)
     @total = 0
